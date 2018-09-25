cd4b31b12d6d2f04152c7158b999aab9a27c2974
Cynth42 <cclinc2005@yahoo.com>
2018-09-25 00:16:21 -0400

    Embed Google Map on screen

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+    componentDidMount() {
+      this.callMap()
+    }
+
+     callMap = () => {
+       loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyARlSeNYJ-pH2neoykbm1cmA8o_6bpdUhY&callback=initMap')
+
+       window.initMap = this.initMap;
+     }
+
+     initMap = () => {
+        const myLatLng = {lat: 40.608494, lng: -74.142372}
+
+        // Create a map object and specify the DOM element
+        // for display.
+        const map = new window.google.maps.Map(document.getElementById('map'), {
+          center: myLatLng,
+          zoom: 14
+        })
+     }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <main>
+       <div id="map"></div>
+      </main>
+    )
   }
 }
 
-export default App;
+/*
+
+</script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
+    async defer></script>
+
+*/
+
+function loadScript(url) {
+
+  const index = window.document.getElementsByTagName('script')[0];
+  const script = window.document.createElement('script');
+  script.defer = true;
+  script.async = true;
+  script.src = url;
+  //script.onerror = window.gm_authFailure
+  index.parentNode.insertBefore(script, index);
+}
+
+export default App
