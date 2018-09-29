0831defea974b5b90e68a64d796703f853071e21
Cynth42 <cclinc2005@yahoo.com>
2018-09-29 02:17:40 -0400

    Animated markers and set up components folder

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ window.gm_authFailure = () => {
 
 class App extends Component {
   state = {
-    venues: []
-    //markers: []
+    venues: [],
+    markers: []
   }
 
   componentDidMount() {
@@ -28,6 +28,7 @@ class App extends Component {
      client_secret: 'D5ZWFILS2RF0SJCA0BZNYKVBCKOYXQO1YJANXFQC3XJG03NZ',
      query: 'food',
      near: 'New York City',
+     //limit: 10,
      v: '20182609' //YYYYDDMM
      }
 
@@ -39,31 +40,38 @@ class App extends Component {
 
     })
     .catch(err => {
-      console.log('Error: ' + err)
+      console.log("Error: " + err)
+      alert("Error occurred while fetching data from four square API!")
     })
   }
 
+//  updateMarkers(){
+//    let listItems = document.getElementsByTagName('li')
+//    let listItemsArray = Array.from(listItems)
+//    let visibleListItems = listItemsArray.filter(li => li.offsetParent!=null)
+//    let listIds = visibleListItems.map(item => item.getAttribute('id'))
+//  }
 
   initMap = () => {
+
     let myLatLng = {lat: 40.7128, lng: -74.0060}
       // Create a map object and specify the DOM element
       // for display.
     let myMap = new window.google.maps.Map(document.getElementById("map"), {
       center: myLatLng,
       zoom: 16
+
     })
 
     //create InfoWindow
     let infoWindow = new window.google.maps.InfoWindow()
     //Looping thru venues array to generate markers
     this.state.venues.map(myVenue => {
+      console.log(myVenue)
+      let name = `${myVenue.venue.name}`
+      let address = `${myVenue.venue.location.formattedAddress}`
 
-        console.log(myVenue)
-        let name = `${myVenue.venue.name}`
-        let address = `${myVenue.venue.location.formattedAddress}`
-
-        let contentString = `<div>
-                              <img id='img'>
+      let contentString = `<div>
                               <h3>${name}</h3>
                               <p>${address}</p>
                              </div>`
@@ -71,20 +79,41 @@ class App extends Component {
 
      //Create a marker and set its position for each venue.
       let myMarker = new window.google.maps.Marker({
-       map: myMap,
-       position: {lat: myVenue.venue.location.lat, lng: myVenue.venue.location.lng},
-       title: myVenue.venue.name,
+        map: myMap,
+        position: {lat: myVenue.venue.location.lat, lng: myVenue.venue.location.lng},
+        title: myVenue.venue.name,
+        //draggable = true
+        animation: window.google.maps.Animation.DROP
       })
+
+      function toggleBounce(marker) {
+        marker.setAnimation(window.google.maps.Animation.BOUNCE)
+        //if (marker.getAnimation() !== null) {
+        //  marker.setAnimation(null);
+        //} else {
+      //  marker.setAnimation(window.google.maps.Animation.BOUNCE)
+        //}
+        setTimeout(() => {
+          marker.setAnimation(null)
+        }, 1500)
+      }
 
       //Click on a marker
       myMarker.addListener('click', () => {
+        console.log(myMarker)
+        toggleBounce(myMarker)
+
         //Change the content
         infoWindow.setContent(contentString)
 
         //Open an InfoWindow
         infoWindow.open(myMap, myMarker)
       })
+      this.setState({
+      markers: [...this.state.markers, myMarker]
+      })
     })
+
   }
 
   render() {
@@ -92,10 +121,20 @@ class App extends Component {
       <main>
         <div id="map"></div>
       </main>
+      <div className="container-fluid app-wrap">
+        <footer className="app-footer">
+          <p className="footer-title">
+            copyright (c) 2018{" "}
+            <a href="/">
+            <strong>Neighborhood Maps</strong>
+            </a>{" "}
+            All Rights Reserved.
+          </p>
+        </footer>
+      </div>
     )
   }
 }
-
 
 /**
  * Asynchronously loads JavaScript <script> tags on a page.
@@ -109,7 +148,9 @@ class App extends Component {
    script.async = true
    script.src = url
    script.onerror = window.gm_authFailure
-   index.parentNode.insertBefore(script, index)
+   document.body.appendChild(script, index)
+   //index.parentNode.insertBefore(script, index)
  }
+
 
  export default App
---
+++ src/components/navBar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+class NavBar extends Component {
+
+   render() {
+     return(
+       <div id="navbar" aria-role="navigation">
+        <h3 id="haeading" tabindex="0">Neighborhood Maps</h3>
+     )
+
+   }
